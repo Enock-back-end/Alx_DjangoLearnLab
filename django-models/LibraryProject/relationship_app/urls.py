d1d39de5e3833d3061f76db52bc553a5cbcb9929
Enock-back-end <enockshabangu2028@gmail.com>
2025-07-17 16:04:13 +0200

    Role-Based Access

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,6 +4,11 @@ from . import views
 from .views import LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 
+from django.urls import path
+from .views.admin_view import admin_view
+from .views.librarian_view import librarian_view
+from .views.member_view import member_view
+
 
 urlpatterns = [
     path('books/', list_books_view, name='list_books'),
@@ -20,6 +25,11 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
     
 
     
