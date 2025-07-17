ce4e17ea9da8217c2c08e6b1565d6e2efeb90332
Enock-back-end <enockshabangu2028@gmail.com>
2025-07-17 14:38:02 +0200

    implementing advanced relationships

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,7 +5,8 @@ def list_books_in_library(library_name):
     return library.books.all()
 
 def query_books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
