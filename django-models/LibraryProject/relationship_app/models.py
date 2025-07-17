bc7457d6d61b587ab64021a5105ccf948ec9b8e9
Enock-back-end <enockshabangu2028@gmail.com>
2025-07-17 14:27:22 +0200

    implementing advanced relationships

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-from .models import Library
 
-def list_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.book_set.all()
-
-
-# Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -22,7 +15,7 @@ class Book(models.Model):
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book)
+    books = models.ManyToManyField(Book, related_name='libraries')  # fixed
 
     def __str__(self):
         return self.name
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,14 @@
-from relationship_app.models import Author, Book, Library, Librarian
-from .models import Book
+from .models import Library, Book
+
+
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.book_set.all()
 
 def books_by_author(author_name):
     return Book.objects.filter(author=author_name)
 
-author = Author.objects.get(name="J.K. Rowling")
-books_by_author = Book.objects.filter(author=author)
 
-library = Library.objects.get(name="Central Library")
-books_in_library = library.books.all()
-
-librarian = Librarian.objects.get(library=library)
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
