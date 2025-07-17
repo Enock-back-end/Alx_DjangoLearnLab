31261d5bf8b37b7502aae01df7568b70f7c84e19
Enock-back-end <enockshabangu2028@gmail.com>
2025-07-17 16:39:00 +0200

    Role-Based Access

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
+
 # Register your models here.
+from django.contrib import admin
+from .models import UserProfile
 
-
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'admin_view.html')
+admin.site.register(UserProfile)
