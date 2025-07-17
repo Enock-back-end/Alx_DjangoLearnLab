862234d21a9ecfeba3999ddddbc167951fa850e8
Enock-back-end <enockshabangu2028@gmail.com>
2025-07-17 15:37:04 +0200

    User Authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,18 +2,25 @@ from django.urls import path
 from .views import list_books_view, LibraryDetailView
 from . import views
 from .views import LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path('books/', list_books_view, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-        path('login/', views.user_login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-        path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    
+
+
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    
 
     
 ]
