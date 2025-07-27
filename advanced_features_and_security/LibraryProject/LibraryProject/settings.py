1cd45e2bad5616cd8cbd19a00d2772168f7e24d2
Enock-back-end <enockshabangu2028@gmail.com>
2025-07-27 14:56:22 +0200

    custom user model update

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
 
 DEBUG = False  # In production
 SECURE_BROWSER_XSS_FILTER = True
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,9 +3,21 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Book
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Show these fields in the list view
     search_fields = ('title', 'author')  # Search by title or author
     list_filter = ('publication_year',)  # Add a filter by year
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
