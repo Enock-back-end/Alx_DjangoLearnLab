f64448036307dbc61dabf8713ebdfeb006941fdc
Enock-back-end <enockshabangu2028@gmail.com>
2025-08-17 12:37:38 +0200

    Update DATABASES config to include USER and PORT for SQLite

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -123,9 +123,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-INSTALLED_APPS = [
-    ...,
-    'blog',
-]
+
+
 
 STATIC_URL = '/static/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '', 
+    }
+}
+
+USER = ''
+PORT = ''
