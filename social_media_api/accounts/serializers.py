ce1827f7dbb417753eb1553820d8619b34c496cf
Enock-back-end <enockshabangu2028@gmail.com>
2025-08-24 23:40:46 +0200

    Added custom user model

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,15 +1,16 @@
+
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password", "bio", "profile_picture")
+        fields = ["id", "username", "email", "password", "bio", "profile_picture"]
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -19,20 +20,6 @@ class RegisterSerializer(serializers.ModelSerializer):
             bio=validated_data.get("bio", ""),
             profile_picture=validated_data.get("profile_picture", None),
         )
+        # token
+        Token.objects.create(user=user)
         return user
-
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-
-
-class LoginResponseSerializer(serializers.Serializer):
-    refresh = serializers.CharField()
-    access = serializers.CharField()
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("id", "username", "email", "bio", "profile_picture", "followers")
