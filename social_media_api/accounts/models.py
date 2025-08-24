ecaf680247f7425100b5bb793a46428de41c674a
Enock-back-end <enockshabangu2028@gmail.com>
2025-08-24 22:59:39 +0200

    Added custom user model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
-
-class User(AbstractUser):
-    bio = models.TextField(blank=True)
-    profile_picture = models.URLField(blank=True)
-    following = models.ManyToManyField(
-        "self", symmetrical=False, related_name="followers", blank=True
-    )
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    followers = models.ManyToManyField("self", symmetrical=False, related_name="following", blank=True)
 
     def __str__(self):
-        return self.username 
+        return self.username
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,37 +5,42 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
-# Create your views here.
 from .serializers import RegisterSerializer, UserSerializer
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, UserSerializer, LoginResponseSerializer
+# Create your views here.
+
+
 
 User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = User.objects.get(username=response.data['username'])
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            "user": UserSerializer(user).data,
-            "token": token.key
-        })
 
-class LoginView(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        token = Token.objects.get(key=response.data['token'])
-        return Response({
-            "user": UserSerializer(token.user).data,
-            "token": token.key
-        })
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginResponseSerializer
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {"refresh": str(refresh), "access": str(refresh.access_token)},
+                status=status.HTTP_200_OK,
+            )
+        return Response({"error": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class ProfileView(generics.RetrieveUpdateAPIView):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
-        return self.request.user 
+        return self.request.user
